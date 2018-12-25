f3c3c6f09dce3dc035de7ad55a41e7313dcac797
me <bhavesh.pareek36@gmail.com>
2018-12-25 16:32:23 +0530

    service worker update

--- static/js/sw.js
+++ static/js/sw.js
@@ -27,48 +27,55 @@ self.addEventListener("activate", event => {
 });
 
 self.addEventListener("fetch", event => {
-  const request = event.request;
-  // Ignore not GET request.
-  if (request.method !== "GET") {
-    console.log(`[SW] Ignore non GET request-> ${request.method}`);
-    return;
-  }
-  const reqURL = new URL(request.url);
-  if (reqURL.hostname !== self.location.hostname) {
-    return;
-  }
-  if (reqURL.pathname === "/api/gen_204/" && !self.navigator.onLine) {
-    event.respondWith(new Response("", { status: 200 }));
-  }
-
-  const resource = global.caches.match(request).then(response => {
-    if (response) {
-      console.log(`[SW] fetch URL ${reqURL.href} from cache`);
-      return response;
-    }
-    // Load and cache known assets.
-    return fetch(request)
-      .then(resp => {
-        if (!resp || !resp.ok) {
-          console.log(
-            `[SW] URL [${reqURL.href}] wrong responseNetwork: ${resp.status} ${
-              resp.type
-            }`
-          );
-
-          return resp;
-        }
-        console.log(`[SW] Fetched: ${reqURL.href}`);
-        return resp;
-      })
-      .catch(() => {
-        // User is landing on our page.
+  event.respondWith(
+    caches.match(event.request).then(
+      (response = response || fetch(event.request)).catch(() => {
         if (event.request.mode === "navigate") {
-          console.log("navigation");
-          event.respondWith(global.caches.match("./"));
+          return caches.match("./");
         }
-        return null;
-      });
-  });
-  event.respondWith(resource);
+      })
+    )
+  );
+  //   const request = event.request;
+  //   // Ignore not GET request.
+  //   if (request.method !== "GET") {
+  //     console.log(`[SW] Ignore non GET request-> ${request.method}`);
+  //     return;
+  //   }
+  //   const reqURL = new URL(request.url);
+  //   if (reqURL.hostname !== self.location.hostname) {
+  //     return;
+  //   }
+  //   if (reqURL.pathname === "/api/gen_204/" && !self.navigator.onLine) {
+  //     event.respondWith(new Response("", { status: 200 }));
+  //   }
+  //   const resource = global.caches.match(request).then(response => {
+  //     if (response) {
+  //       console.log(`[SW] fetch URL ${reqURL.href} from cache`);
+  //       return response;
+  //     }
+  //     // Load and cache known assets.
+  //     return fetch(request)
+  //       .then(resp => {
+  //         if (!resp || !resp.ok) {
+  //           console.log(
+  //             `[SW] URL [${reqURL.href}] wrong responseNetwork: ${resp.status} ${
+  //               resp.type
+  //             }`
+  //           );
+  //           return resp;
+  //         }
+  //         console.log(`[SW] Fetched: ${reqURL.href}`);
+  //         return resp;
+  //       })
+  //       .catch(() => {
+  //         // User is landing on our page.
+  //         if (event.request.mode === "navigate") {
+  //           console.log("navigation");
+  //           event.respondWith(global.caches.match("./"));
+  //         }
+  //         return null;
+  //       });
+  //   });
+  //   event.respondWith(resource);
 });
